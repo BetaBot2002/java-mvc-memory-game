c5bcb40dbe25c0ec137ff17eccee0ac1923c1454
Sinchan Nandy <105705266+BetaBot2002@users.noreply.github.com>
2024-08-12 01:32:02 +0530

    Game.java initialized

--- src/model/Game.java
+++ src/model/Game.java
@@ -1,6 +1,69 @@
 package model;
 
-public class Game {
+import java.util.ArrayList;
+import java.util.List;
 
-    
+public class Game {
+    private Deck deck;
+    private Card firstSelectedCard;
+    private Card secondSelectedCard;
+    private int score;
+    private List<Card> matchedCards;
+
+    public Game(int numberOfUniqueCards) {
+        this.deck = new Deck(numberOfUniqueCards);
+        this.matchedCards = new ArrayList<>();
+        this.score = 0;
+    }
+
+    public boolean selectCard(int index) {
+        Card selectedCard = deck.getCard(index);
+
+        if (selectedCard == null || !selectedCard.isFacedDown() || matchedCards.contains(selectedCard)) {
+            return false;
+        }
+
+        selectedCard.flip();
+
+        if (firstSelectedCard == null) {
+            firstSelectedCard = selectedCard;
+        } else {
+            secondSelectedCard = selectedCard;
+            checkMatch();
+        }
+
+        return true;
+    }
+
+    private void checkMatch() {
+        if (firstSelectedCard != null && secondSelectedCard != null) {
+            if (firstSelectedCard.equals(secondSelectedCard)) {
+                score++;
+                matchedCards.add(firstSelectedCard);
+                matchedCards.add(secondSelectedCard);
+            } else {
+                firstSelectedCard.flip();
+                secondSelectedCard.flip();
+            }
+
+            firstSelectedCard = null;
+            secondSelectedCard = null;
+        }
+    }
+
+    public boolean isGameOver() {
+        return deck.remainingCards() <= 2;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public List<Card> getMatchedCards() {
+        return matchedCards;
+    }
+
+    public Deck getDeck() {
+        return deck;
+    }
 }
